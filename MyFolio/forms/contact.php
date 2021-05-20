547fb0abff5624d333f290c733a93d8ab459738e
Salaydinov007 <kemirovtursunboj@gmail.com>
2021-05-20 08:16:18 +0500

    unimportant things deleted
    extra objects

--- MyFolio/forms/contact.php
+++ MyFolio/forms/contact.php
@@ -24,14 +24,12 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
